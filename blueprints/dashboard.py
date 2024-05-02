8366c7c0e4c866d9244e4c6c42954ba1c14a6451
HardoMX <hardo314@gmail.com>
2024-05-02 13:02:23 +0200

    Add weather information

--- blueprints/dashboard.py
+++ blueprints/dashboard.py
@@ -1,12 +1,18 @@
 from flask import Blueprint, render_template
 from datetime import datetime
+from pyowm import OWM
+from pyowm.utils import config
+from pyowm.utils import timestamps
 
 dash_bp = Blueprint('dash', __name__)
+
+owm = OWM('3c0c32b6f7e63b1002244e6dc8a0c736')
+mgr = owm.weather_manager()
 
 @dash_bp.route("/", methods=["GET"])
 def dash():
     # TODO: figure out how to dynamically update time without page refresh
-    time = datetime.now().strftime("%H:%M:%S")
+    time = datetime.now().strftime("%H:%M")
     date = datetime.now().strftime("%B %d, %Y")
 
     if int(datetime.now().strftime("%H")) < 12:
@@ -17,5 +23,8 @@ def dash():
         time_of_day = "evening"
     else:
         time_of_day = "night"
+        
+    weather = mgr.weather_at_place('Laxa,SE').weather.detailed_status
+    temp = mgr.weather_at_place('Laxa,SE').weather.temperature('celsius')
 
-    return render_template("dashboard.html", title="Dashboard", time=time, date=date, time_of_day=time_of_day)
+    return render_template("dashboard.html", title="Dashboard", time=time, date=date, time_of_day=time_of_day, weather=weather, temp=temp['temp'])
