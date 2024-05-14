0001edd49638f8c7818e4417d05705684f58cb32
HardoMX <andreas@andreashard.tech>
2024-05-14 15:05:34 +0200

    Start database

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
 from factory import create_app
+from flask_sqlalchemy import SQLAlchemy
 
 socketio, app = create_app()
 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
 
 if __name__ == "__main__":
     socketio.run(app, debug=True, host="0.0.0.0", port=3000, use_reloader=True, log_output=False)
---
+++ models.py
@@ -0,0 +1,36 @@
+from app import db
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(50), index=True, unique = True)
+    sites_list = db.Column(db.Integer, db.ForeignKey('sitelist.id'))
+
+    def __repr__(self):
+        return f"{self.username}"
+
+class Site(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(50), index=True)
+    url = db.Column(db.String(50), index=True)
+    icon = db.Column(db.String(50), index=True)
+    n = db.Column(db.Integer, index=False, unique=False)
+
+    def __repr__(self):
+        return f"Title: {self.title}, url: {self.url}, icon: {self.icon}"
+
+class Item(db.Model): # What is this?
+    id = db.Column(db.Integer, primary_key=True)
+    site_id = db.Column(db.Integer, db.ForeignKey('site.id'))
+    sites_list = db.Column(db.Integer, db.ForeignKey('sitelist.id'))
+
+    def __repr__(self):
+        return f"{Site.query.get(self.site_id)}"
+
+class Sitelist(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), index=True)
+    items = db.relationship('Item', backref='sitelist', lazy='dynamic')
+
+    def __repr__(self):
+        return f"Sitelist {self.id}"
+
