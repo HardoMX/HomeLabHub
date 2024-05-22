84ab8bf795c3ddc45fb12e5865f54124bc8ca248
HardoMX <andreas@andreashard.tech>
2024-05-22 08:17:25 +0200

    Add flag for reloading if settings.yaml is changed

--- app.py
+++ app.py
@@ -3,4 +3,4 @@ from factory import create_app
 app = create_app()
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=3000, use_reloader=True)
+    app.run(debug=True, host="0.0.0.0", port=3000, use_reloader=True, extra_files=['./blueprints/data/settings.yaml'])
